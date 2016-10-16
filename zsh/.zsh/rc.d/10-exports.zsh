9aaa6b195bff0407c4cc27393bedbc8897cf1878
Mel Boyce <syngin@me.com>
2016-10-17 08:04:53 +1100

    added export for rxvt socket

--- zsh/.zsh/rc.d/10-exports.zsh
+++ zsh/.zsh/rc.d/10-exports.zsh
@@ -13,6 +13,7 @@ export MOST_EDITOR="nvim %s +%d"
 export PAGER=most
 export PATH="$PATH:$HOME/bin"
 export PS_FORMAT="pid,user,group,nice,pri,psr,ppid,start,rss,stat,command"
+export RXVT_SOCKET="/home/mel/.rxvt.sock"
 export S_COLORS="always"
 export SUDO_EDITOR="rvim"  # no shell commands, no suspend
 export TEMP="$TMP"
