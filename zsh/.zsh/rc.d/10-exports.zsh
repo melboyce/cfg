5074e658c90d1987590e76c197515f6499e328ad
Mel Boyce <syngin@me.com>
2016-10-20 21:10:04 +1100

    tweaked exports

--- zsh/.zsh/rc.d/10-exports.zsh
+++ zsh/.zsh/rc.d/10-exports.zsh
@@ -7,16 +7,17 @@ mkdir -p $TMP
 
 export BROWSER="/usr/bin/qutebrowser"
 export EDITOR="nvim"
-export GREP_COLOR='31'
+export GREP_COLOR=31
 export MANPATH="/usr/share/man:/usr/local/share/man:/usr/local/man"
 export MOST_EDITOR="nvim %s +%d"
-export PAGER=most
+export PAGER="most"
 export PATH="$PATH:$HOME/bin"
 export PS_FORMAT="pid,user,group,nice,pri,psr,ppid,start,rss,stat,command"
 export RXVT_SOCKET="/home/mel/.rxvt.sock"
+export SUDO_EDITOR="nvim -Z"  # no shell commands, no suspend (not sure if neovim disables suspend)
 export S_COLORS="always"
-export SUDO_EDITOR="rvim"  # no shell commands, no suspend
 export TEMP="$TMP"
+export TIMEZONE=$TZ
 export TMP="$TMP"
 export TMPDIR="$TMP"
 export TZ="Australia/Melbourne"
