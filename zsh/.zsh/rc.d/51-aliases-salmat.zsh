7492c07d835e549ed0c1d13d99caa70b458457d6
Mel Boyce <mel.boyce@myob.com>
2017-03-30 09:14:00 +1100

    removes redundant aliases

--- zsh/.zsh/rc.d/51-aliases-salmat.zsh
+++
@@ -1,5 +0,0 @@
-# zshrc - salmat-specific aliases
-
-# remote desktop
-alias rdw="rdesktop -g 1440x900 -a 32 -5 -K -P -z -x l -r sound:off -r disk:wntrfll=/home/mel -r clipboard:PRIMARYCLIPBOARD -u boycem -d SALMAT frkv000010"
-alias rd="rdesktop -g 1280x800 -a 32 -P -z -x l -r sound:off -r disk:wntrfll=/home/mel -u boycem -d SALMAT"
