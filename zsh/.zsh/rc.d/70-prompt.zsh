f6d7f45ce6d0fdf3716cb751e7b3d5b1cb8e455a
Mel Boyce <syngin@me.com>
2017-04-21 22:52:33 +1000

    tweaky

--- zsh/.zsh/rc.d/70-prompt.zsh
+++ zsh/.zsh/rc.d/70-prompt.zsh
@@ -44,9 +44,9 @@ add-zsh-hook chpwd prompt_chpwd
 branchcolor="%{%F{030}%}"
 sepcolor="%{%F{058}%}"
 zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:*' stagedstr ' %{%K{236}%}%{%F{046}%}┄'
-zstyle ':vcs_info:*' unstagedstr '%{%F{196}%}┄%{%k%f%}'
-zstyle ':vcs_info:git*' formats "${branchcolor}%b${rs}%c%u"
+zstyle ':vcs_info:*' stagedstr '%{%K{236}%}%{%F{046}%}┄%{%k%f%}'
+zstyle ':vcs_info:*' unstagedstr '%{%K{236}%}%{%F{196}%}┄%{%k%f%}'
+zstyle ':vcs_info:git*' formats "${branchcolor}%b${rs} %c%u"
 
 PS1='${userprompt}${hostprompt} ${pathprompt} ${vcs_info_msg_0_} '
 
