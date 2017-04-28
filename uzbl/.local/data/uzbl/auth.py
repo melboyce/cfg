87781b25700f6f38fee0028ae9322e92f4e6ec07
Mel Boyce <syngin@me.com>
2017-04-29 01:14:17 +1000

    initial import

---
+++ uzbl/.local/data/uzbl/auth.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+
+from __future__ import print_function
+
+import os
+import gtk
+import sys
+import argparse
+
+
+def responseToDialog(entry, dialog, response):
+    dialog.response(response)
+
+
+def getText(authHost, authRealm):
+    dialog = gtk.MessageDialog(
+        None,
+        gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
+        gtk.MESSAGE_QUESTION,
+        gtk.BUTTONS_OK_CANCEL,
+        None)
+    dialog.set_markup('%s at %s' % (authRealm, authHost))
+
+    login = gtk.Entry()
+    password = gtk.Entry()
+    password.set_visibility(False)
+
+    login.connect("activate", responseToDialog, dialog, gtk.RESPONSE_OK)
+    password.connect("activate", responseToDialog, dialog, gtk.RESPONSE_OK)
+
+    hbox = gtk.HBox()
+
+    vbox_entries = gtk.VBox()
+    vbox_labels = gtk.VBox()
+
+    vbox_labels.pack_start(gtk.Label("Login:"), False, 5, 5)
+    vbox_labels.pack_end(gtk.Label("Password:"), False, 5, 5)
+
+    vbox_entries.pack_start(login)
+    vbox_entries.pack_end(password)
+
+    dialog.format_secondary_markup("Please enter username and password:")
+    hbox.pack_start(vbox_labels, True, True, 0)
+    hbox.pack_end(vbox_entries, True, True, 0)
+
+    dialog.vbox.pack_start(hbox)
+    dialog.show_all()
+    rv = dialog.run()
+
+    output = {
+        'username': login.get_text(),
+        'password': password.get_text()
+    }
+    dialog.destroy()
+    return rv, output
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('host')
+    parser.add_argument('realm')
+    parser.add_argument('extra', nargs='*')
+    args = parser.parse_args()
+
+    rv, output = getText(args.host, args.realm)
+    if rv == gtk.RESPONSE_OK:
+        print('AUTH')
+        print(output['username'])
+        print(output['password'])
+    else:
+        print('IGNORE')
+        exit(1)
---
+++ uzbl/.local/data/uzbl/scheme.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+import os
+import subprocess
+import sys
+
+try:
+  import urllib.parse as urlparse
+except ImportError:
+  import urlparse
+
+
+def detach_open(cmd, **kwargs):
+    # Thanks to the vast knowledge of Laurence Withers (lwithers) and this message:
+    # http://mail.python.org/pipermail/python-list/2006-November/587523.html
+    if not os.fork():
+        null = os.open(os.devnull, os.O_WRONLY)
+        for i in range(3):
+            os.dup2(null, i)
+        os.close(null)
+        subprocess.Popen(cmd, **kwargs)
+    print('USED')
+
+
+def main(uri):
+    u = urlparse.urlparse(uri)
+
+    if u.scheme == 'mailto':
+        detach_open(['st', '-e', 'mail', u.path])
+    elif u.scheme == 'xmpp':
+        # Someone check for safe arguments to gajim-remote
+        detach_open(['gajim-remote', 'open_chat', uri])
+    elif u.scheme == 'git':
+        detach_open(['git', 'clone', '--', uri], cwd=os.path.expanduser('~/r'))
+
+
+if __name__ == '__main__':
+    try:
+        uri = sys.argv[1]
+    except IndexError:
+        print('Error: No uri given to handle.')
+
+        os.exit(1)
+
+    main(uri)
