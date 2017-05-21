dbe82845e7cf715cb5640e6e4571211be272f4d4
Mel Boyce <weird@syngin.net>
2017-05-21 22:29:37 +1000

    removed

--- zsh/.zsh/rc.d/81-widgets.zsh
+++
@@ -1,9 +0,0 @@
-# zshrc - widgets
-
-# store a command line in history without executing
-commit-to-history() {
-	print -s ${(z)BUFFER}
-	zle send-break
-}
-zle -N commit-to-history
-bindkey "^\n" commit-to-history
