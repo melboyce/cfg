ae8070f6c04c2ce0237ee1bcc0df0370400b8cd9
Mel Boyce <weird@syngin.net>
2017-06-16 18:39:54 +1000

    removed lolcat

--- zsh/.zsh/rc.d/99-done.zsh
+++ zsh/.zsh/rc.d/99-done.zsh
@@ -3,8 +3,5 @@
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
 source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
-if (( $+commands[toilet] )); then
-    fortune -a | toilet -f term --metal | lolcat
-else
-    fortune -a
-fi
+fortune -a
+echo
