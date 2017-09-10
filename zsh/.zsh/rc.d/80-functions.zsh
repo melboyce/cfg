4b5713344e436c4260da94c6e2b0198950c01070
Mel Boyce <weird@syngin.net>
2017-09-10 19:35:09 +1000

    add git helper func

--- zsh/.zsh/rc.d/80-functions.zsh
+++ zsh/.zsh/rc.d/80-functions.zsh
@@ -1,7 +1,15 @@
 # zshrc - functions
 
-px() { ps uwwp ${$(pgrep -d, "${(j:|:)@}"):?no matches} }
-pstop() { ps -eo pid,user,pri,ni,vsz,rsz,stat,pcpu,pmem,time,comm --sort -pcpu | head -11 }
+px() { ps uwwp ${$(pgrep -d, "${(j:|:)@}"):?no matches}; }
+pstop() { ps -eo pid,user,pri,ni,vsz,rsz,stat,pcpu,pmem,time,comm --sort -pcpu | head -11; }
 
-mkcd() { mkdir -p "$1" && cd "$1" }
+mkcd() { mkdir -p "$1" && cd "$1"; }
 compdef mkcd=mkdir  # completions work like mkdir
+
+gdac() {
+    [[ -e "$1" ]] || { echo "gdac <file>"; return; }
+    git diff "$1"
+    read -rq ?"commit? " || return
+    git add "$1"
+    git commit
+}
