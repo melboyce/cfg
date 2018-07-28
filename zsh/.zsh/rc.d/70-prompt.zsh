da8bac5f7dd66351662053907d880d20747b082e
Mel Boyce <weird@syngin.net>
2018-07-28 22:00:00 +1000

    update icons

--- zsh/.zsh/rc.d/70-prompt.zsh
+++ zsh/.zsh/rc.d/70-prompt.zsh
@@ -2,48 +2,56 @@
 #
 # note: prompt_subst and colors are loaded elsewhere
 
+prompt_icon_path="$HOME/.zsh/prompt-icon"
+
 # colors
 rs="%{%k%f%}"
-safe_username="mel"         # this username will not appear in prompts
-pathcolor="%{%F{069}%}"     # color of the path
-usercolor="%{%F{160}%}"     # if a username is displayed, it is this color
-sepcolor="%{%F{235}%}"      # color of '@' in 'user@host'
-exitcolor="%{%F{196}%}"     # fg color for exit code if non-zero
-vcsiconcolor="%{%F{231}%}"  # vcs icon
-branchcolor="%{%K{236}%F{178}%}"   # vcs branch
-stagedcolor="%{%F{070}%}"   # staged change icon
-unstagedcolor="%{%F{202}%}" # unstaged change icon
-repocolor="%{%F{190}%}"     # repo name
-repopathcolor="%{%F{081}%}" # repo path
-hostcolor="%{%F{190}%}"
+safe_username="mel"              # does not appear in prompt
+hostcolor="%{%F{117}%}"          # the color of the prompt-icon
+pathcolor="%{%F{069}%}"          # color of the path
+usercolor="%{%F{160}%}"          # color of username if displayed
+sepcolor="%{%F{235}%}"           # color of '@' in 'user@host'
+exitcolor="%{%K{232}%F{197}%}"   # color for exit code if non-zero
+vcsiconcolor="%{%F{218}%}"       # vcs icon color
+branchcolor="%{%K{236}%F{178}%}" # vcs branch color
+stagedcolor="%{%F{070}%}"        # vcs staged change icon
+unstagedcolor="%{%F{202}%}"      # vcs unstaged change icon
+repocolor="%{%F{190}%}"          # vcs repo name
+repopathcolor="%{%F{081}%}"      # vcs repo path
+
+# "icons"
+stagedicon=" "   # ready to commit
+unstagedicon=" " # ready to add
+vcsicon="  "     # in vcs directory
+exiticon=" "     # for non-zero exit code RPROMPT
 
 
 # user/host/path
 userprompt=""
-[[ `whoami` != $safe_username ]] && userprompt="${usercolor}%n${rs}${sepcolor}@${rs}"
+[[ $(whoami) != "$safe_username" ]] && userprompt="${usercolor}%n${rs}${sepcolor}@${rs}"
 pathprompt="${pathcolor}%4(c:...:)%3c${rs}"
 
 
 # vcs bits
-stagedstr="${stagedcolor}  "
-unstagedstr="${unstagedcolor}  "
+stagedstr="$stagedcolor $stagedicon"
+unstagedstr="$unstagedcolor $unstagedicon"
 zstyle ':vcs_info:*' disable bzr cdv darcs mtn p4 svk tla
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' get-revision true
-zstyle ':vcs_info:*' stagedstr $stagedstr
-zstyle ':vcs_info:*' unstagedstr $unstagedstr
-zstyle ':vcs_info:git*' formats "${vcsiconcolor}  ${repocolor}%r${repopathcolor}/%S ${branchcolor}%b%c%u${rs}"
+zstyle ':vcs_info:*' stagedstr "$stagedstr"
+zstyle ':vcs_info:*' unstagedstr "$unstagedstr"
+zstyle ':vcs_info:git*' formats "${vcsiconcolor}${vcsicon}${repocolor}%r${repopathcolor}/%S ${branchcolor}%b%c%u${rs}"
 
 
-# precmd - vcs replaces normal prompt if available
+# precmd - vcs replaces normal prompt
 precmd() {
     vcs_info
-    if [[ ! -n ${vcs_info_msg_0_} ]]; then
-        hostprompt="$hostcolor"`cat $HOME/.zsh/prompt-icon`"$rs"
-        PS1='${userprompt}${hostprompt} ${pathprompt} '
-    else
-        PS1='${vcs_info_msg_0_} '
+    if [[ -n ${vcs_info_msg_0_} ]]; then
+        PS1="${vcs_info_msg_0_} "
+        return
     fi
+    hostprompt="$hostcolor$(<"$prompt_icon_path")$rs"
+    PS1="${userprompt}${hostprompt} ${pathprompt} "
 }
 
 
@@ -52,5 +60,5 @@ PS2="%{%F{066}%}%_${rs} %{%F{106}%}>%{%F{112}%}>%{%F{118}%}>${rs} "
 
 
 # rprompt
-exitprompt="%(?..${exitcolor}%?)"
+exitprompt="%(?..${exitcolor}${exiticon}%?)"
 RPROMPT="${exitprompt}"
