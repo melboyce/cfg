4fb9340693801cae2320c3daa6c73bf45ab13fe6
Mel Boyce <weird@syngin.net>
2018-09-06 20:19:27 +1000

    touch up

--- zsh/.zsh/rc.d/50-aliases.zsh
+++ zsh/.zsh/rc.d/50-aliases.zsh
@@ -14,27 +14,27 @@ alias ldt="tree -pughCFd -L 1"
 alias ldtr="tree -pughCFd"
 alias lst="tree"
 
+# regulars
 alias dfl="df -hl --exclude tmpfs --exclude devtmpfs --total"
 alias grep="grep -nH --color=auto"
 alias h="history"
 alias http="http --style monokai"
 alias j="jobs"
-alias killall="pkill"
+alias killall="pkill"  # some killall implementations are violent
 alias ls="ls --color=auto --group-directories-first --classify --time-style='+%Y-%m-%d %H:%M' --human-readable -X"
 alias ll="ls -la"
-alias mt="multitail --follow-all"
 alias psa="ps --forest -e --sort uid"
 alias psc="ps xawf -eo pid,user,cgroup,args"
 alias pse="ps -ef"
 alias pt="pstree -lnG"
 alias pta="pstree -lapG"
 alias s="ssh -4"
-alias svst="sudo sv status /var/service/*"
 alias watch="watch --color"
 
 # void
 alias xi="sudo xbps-install"
 alias xq="sudo xbps-query"
+alias svst="sudo sv status /var/service/*"
 
 # optionals
 [[ -x /usr/bin/xstow ]] && alias stow="xstow"
