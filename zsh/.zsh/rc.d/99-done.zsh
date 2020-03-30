1e1f071edbe6fe358c0ac6b207e9f6413cbc23c8
Mel Boyce <weird@syngin.net>
2020-03-30 21:01:57 +1100

    tweak

--- zsh/.zsh/rc.d/99-done.zsh
+++ zsh/.zsh/rc.d/99-done.zsh
@@ -1,11 +1,8 @@
 # zshrc - done
 
-if hash toilet 2>/dev/null; then
-    fortune -a | toilet -f future -S --gay
-else
-    fortune -a
-fi
-echo
+f=$(fortune -s)
+hash toilet 2>/dev/null && toilet -f future -S --gay <<<$f
+echo $f; echo
 
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
 source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
