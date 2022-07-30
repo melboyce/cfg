39b89748d8eb1e0e3f2959289a2d9c3cfca31d2f
Mel Boyce <mel@littlepay.com>
2022-07-30 14:33:54 +1000

    update

--- zsh/.zsh/rc.d/10-exports.zsh
+++ zsh/.zsh/rc.d/10-exports.zsh
@@ -1,8 +1,8 @@
 # zshrc - exports
 
 # paths
-export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/Library/Python/3.10/bin"
-export MANPATH="/usr/share/man:/usr/local/share/man:/usr/local/man"
+export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/Library/Python/3.10/bin:$HOME/.rd/bin"
+export MANPATH="/opt/local/share/man:/usr/share/man:/usr/local/share/man:/usr/local/man"
 export TMP="$HOME/tmp"
 export TEMP="$TMP"
 export TMPDIR="$TMP"
@@ -10,21 +10,15 @@ export TMPPREFIX="$TMP/"
 
 # program associations
 export EDITOR=kak
-export TERMINAL=alacritty
 export MOST_EDITOR="kak %s +%d"
-export SUDO_EDITOR="nvim -Z"  # no shell commands, no suspend (not sure if neovim disables suspend)
 export VISUAL=kak
-export BROWSER=/usr/bin/firefox
-xdpyinfo >/dev/null 2>&1 || export BROWSER="/usr/bin/lynx"
+export BROWSER=open
 
 # program options
 export GREP_COLOR=31
 export PAGER=most
 export PS_FORMAT=pid,user,group,nice,pri,psr,ppid,start,rss,stat,command
 export S_COLORS=always
-export NNN_USE_EDITOR=1
-export HUBR_DEFAULT_ORG=myob-ops
-export PIPE_BRANCH=main
 
 # l10n
 export TZ=Australia/Melbourne
@@ -39,14 +33,14 @@ GPG_TTY=$(tty)
 export GPG_TTY
 
 # xdg
-export XDG_DESKTOP_DIR="$HOME"
-export XDG_DOCUMENTS_DIR="$HOME/w"
-export XDG_DOWNLOAD_DIR="$HOME/d"
-export XDG_MUSIC_DIR="$HOME/m"
-export XDG_PICTURES_DIR="$HOME/i"
+export XDG_DESKTOP_DIR="$HOME/Desktop"
+export XDG_DOCUMENTS_DIR="$HOME/Documents"
+export XDG_DOWNLOAD_DIR="$HOME/Downloads"
+export XDG_MUSIC_DIR="$HOME/Music"
+export XDG_PICTURES_DIR="$HOME/Pictures"
 export XDG_RUNTIME_DIR="$HOME/run"
-export XDG_TEMPLATES_DIR="$HOME/x"
-export XDG_VIDEOS_DIR="$HOME/v"
+export XDG_TEMPLATES_DIR="$HOME/tpl"
+export XDG_VIDEOS_DIR="$HOME/Videos"
 
 
 # local exports
